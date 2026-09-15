a349f0fa9b45b93643b06b492a6dcf1ce608d9c4
Nicolas Fajardo <nicolasfajardoplaz26@gmail.com>
2026-09-15 12:27:32 -0500

    Actualizacion controlador

--- demo/demo/src/main/java/com/example/demo/controlador/EstudianteController.java
+++ demo/demo/src/main/java/com/example/demo/controlador/EstudianteController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controlador;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Arrays;
@@ -17,4 +19,7 @@ public class EstudianteController {
  new Estudiante(3L, "Luis Torres", "Ingeniería Industrial")
  );
  }
+ @PostMapping("/estudiantes")
+public Estudiante crearEstudiante(@RequestBody Estudiante nuevoEstudiante)
+{ return nuevoEstudiante; }
 }
