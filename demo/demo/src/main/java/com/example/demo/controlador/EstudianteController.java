5b14639754e755fbe6bebbb796c3cac6896f140e
Nicolas Fajardo <nicolasfajardoplaz26@gmail.com>
2026-09-16 08:12:02 -0500

    Final final

--- demo/demo/src/main/java/com/example/demo/controlador/EstudianteController.java
+++ demo/demo/src/main/java/com/example/demo/controlador/EstudianteController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Arrays;
 import java.util.List;
 import com.example.demo.modelo.Estudiante;
-//
 @RestController
 @RequestMapping("/api")
 public class EstudianteController {
